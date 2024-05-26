dc729e9093c94a1d9e7f787b077928654ad77d71
Artem Krachulov <artem.krachulov@gmail.com>
2024-05-26 14:31:04 +0300

    ♻️Rename ViewController on HomeViewController

--- CurrencyConverter/Sources/Home/HomeViewController.swift
+++ CurrencyConverter/Sources/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CurrencyConverter
 //
 //  Created by Artem Krachulov on 26.05.2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
